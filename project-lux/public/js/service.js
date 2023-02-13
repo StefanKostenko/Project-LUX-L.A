c5c395d67b888bfc4921333cd9f27c8e3f105c89
StefanKostenko <kostenkostephan@gmail.com>
2023-02-13 12:53:25 +0100

    service cambiado a jquery

--- project-lux/public/js/service.js
+++ project-lux/public/js/service.js
@@ -1,16 +1,7 @@
-function resaltar(){
-    this.style.boxShadow= "0px 12px 22px 1px #592a61";
-}
-
-function reset(){
-    this.style.boxShadow = "0 .3rem 5rem rgba(0, 0 , 0, .2)";
-}
-
-window.onload = function(){
-    var elementos = document.getElementsByClassName("box");
-    for(var i=0; i < elementos.length; i++){
-        elementos[i].addEventListener("mouseover", resaltar);
-        elementos[i].addEventListener("mouseout", reset);
-    }
-
-}
+$(document).ready(function(){
+    $(".box").mouseenter(function(){
+            $(this).css("box-shadow", "0px 12px 22px 1px #592a61");
+        }).mouseleave(function(){
+            $(this).css("box-shadow", "0 .3rem 5rem rgba(0, 0 , 0, .2)");
+        });
+});
